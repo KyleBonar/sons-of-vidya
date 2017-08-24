4a31cf465a85cb2481b7d358bf1821903ae021ef
Kyle Bonar <kylewb2011@gmail.com>
2017-08-24 12:18:51 -0500

    Pull in styling and adding comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//main component for app
 import Main from "./components/main/Main";
+
+//pull in styles
+import "./scss/style.scss";
 
 ReactDOM.render(<Main />, document.getElementById("app"));
