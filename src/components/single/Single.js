dffd180d4eba43bc397eabef072c557f6edeaf91
Kyle Bonar <kylewb2011@gmail.com>
2017-08-25 13:13:10 -0500

    Init commit.

---
+++ src/components/single/Single.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Single extends Component {
+  render() {
+    return (
+      <section id="hero">
+        <div className="laughin-clapin">Laughin and Clappin'</div>
+        <div className="video-container">Video here</div>
+      </section>
+    );
+  }
+}
+
+module.exports = Single;
