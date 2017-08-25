40734fe7d81c23cb92baf0f52a2020a43c09c746
Kyle Bonar <kylewb2011@gmail.com>
2017-08-25 15:20:33 -0500

    Added star images, title section.

--- src/components/single/Single.js
+++ src/components/single/Single.js
@@ -2,10 +2,22 @@ import React, { Component } from "react";
 
 class Single extends Component {
   render() {
+    const star = require("../../images/star.png");
     return (
       <section id="hero">
-        <div className="laughin-clapin">Laughin and Clappin'</div>
-        <div className="video-container">Video here</div>
+        <div className="title-holder">
+          <img src={star} className="star" />
+          <img src={star} className="star" />
+          <img src={star} className="star" />
+          <span className="title">Featured Video</span>
+          <img src={star} className="star" />
+          <img src={star} className="star" />
+          <img src={star} className="star" />
+        </div>
+        <div className="container">
+          <div className="laughin-clapin">Laughin and Clappin'</div>
+          <div className="video-container">Video here</div>
+        </div>
       </section>
     );
   }
