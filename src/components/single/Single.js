3607288d4b373e54903a325a76387d0775d40d23
Kyle Bonar <kylewb2011@gmail.com>
2017-08-28 15:47:22 -0500

    Set up to eventually grab youtube API information. Still playing with the html though.

--- src/components/single/Single.js
+++ src/components/single/Single.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 
 class Single extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      key: "AIzaSyALj1_1OHf-JhgtvZhl4xbKaY0A7GcjNd4"
+    };
+
+    this.getLatestYoutube = this.getLatestYoutube.bind(this);
+  }
+
+  componentDidMount() {
+    this.getLatestYoutube();
+  }
+
   render() {
     const star = require("../../images/star.png");
     return (
-      <section id="hero">
+      <section id="single">
         <div className="title-holder">
           <img src={star} className="star" />
           <img src={star} className="star" />
@@ -14,13 +28,44 @@ class Single extends Component {
           <img src={star} className="star" />
           <img src={star} className="star" />
         </div>
-        <div className="container">
-          <div className="laughin-clapin">Laughin and Clappin'</div>
-          <div className="video-container">Video here</div>
+        <div className="content">
+          <div className="description-holder">
+            <div className="video-title">Laughin' and Clappin'</div>
+            <a href="https://www.youtube.com/user/SonsOfVidya" target="_blank">
+              Subscribe on Youtube
+            </a>
+            <a href="https://www.patreon.com/sonsofvidya" target="_blank">
+              Become A Patron
+            </a>
+          </div>
+          <div className="latest-holder">
+            <span>Latest Video</span>
+            <div className="video-holder">
+              <iframe
+                id="ytplayer"
+                type="text/html"
+                src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0&origin=http://example.com"
+                frameborder="0"
+              />
+            </div>
+          </div>
         </div>
       </section>
     );
   }
+
+  getLatestYoutube() {}
 }
 
 module.exports = Single;
+
+{
+  /* 
+  
+        <div className="container">
+          <div className="laughin-clapin">Laughin and Clappin'</div>
+          <div className="video-container">
+            
+          </div>
+        </div> */
+}
