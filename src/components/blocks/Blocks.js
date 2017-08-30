5f995e3a3e7d035cf200e1f1a7734f0cbbf55e69
Kyle Bonar <kylewb2011@gmail.com>
2017-08-30 15:27:13 -0500

    Building out component.

--- src/components/blocks/Blocks.js
+++ src/components/blocks/Blocks.js
@@ -2,7 +2,41 @@ import React, { Component } from "react";
 
 class Row1 extends Component {
   render() {
-    return <div className="container">yo</div>;
+    return (
+      <div className="block-row-teal">
+        <div className="container">
+          <div className="block">
+            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+          </div>
+          <div className="block middle">
+            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+          </div>
+          <div className="block">
+            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Row2 extends Component {
+  render() {
+    return (
+      <div className="block-row-orange">
+        <div className="container">
+          <div className="block">
+            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+          </div>
+          <div className="block middle">
+            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+          </div>
+          <div className="block">
+            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -11,7 +45,7 @@ class Blocks extends Component {
     return (
       <section id="blocks">
         <Row1 />
-        {/* <Row2 /> */}
+        <Row2 />
       </section>
     );
   }
