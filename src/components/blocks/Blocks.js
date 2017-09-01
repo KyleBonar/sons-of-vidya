3f91e310fd46744b184b8f8e3434113ec19b826a
Kyle Bonar <kylewb2011@gmail.com>
2017-09-01 16:14:23 -0500

    Bringing in other badge images and playing around with text.

--- src/components/blocks/Blocks.js
+++ src/components/blocks/Blocks.js
@@ -2,17 +2,22 @@ import React, { Component } from "react";
 
 class Row1 extends Component {
   render() {
+    const badgeQuality = require("../../images/badge_quality_250x250.png");
+    const LaughsAndClaps = require("../../images/badge_laughs_and_claps_250x250.png");
     return (
       <div className="block-row-teal">
         <div className="container">
           <div className="block">
-            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+            <img src={badgeQuality} />
           </div>
           <div className="block middle">
-            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+            <div className="the-difference">
+              <span className="fontFancy">What is the</span>
+              <span className="fontPrimary">Sons of Vidya Difference</span>
+            </div>
           </div>
           <div className="block">
-            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+            <img src={LaughsAndClaps} />
           </div>
         </div>
       </div>
@@ -22,14 +27,16 @@ class Row1 extends Component {
 
 class Row2 extends Component {
   render() {
+    const oneShop = require("../../images/badge_one_shop_250x250.png");
+    const owned = require("../../images/badge_owned_250x250.png");
     return (
       <div className="block-row-orange">
         <div className="container">
           <div className="block">
-            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+            <img src={oneShop} />
           </div>
           <div className="block middle">
-            <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
+            <img src={owned} />
           </div>
           <div className="block">
             <img src="http://localhost:8080/src/images/badge_quality_250x250.png" />
