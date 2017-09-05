a5ce6aba1d356f03b6a5090b083645107c792083
Kyle Bonar <kylewb2011@gmail.com>
2017-09-05 10:06:36 -0500

    Init commit.

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import SectionTitles from "../sectionTitles/SectionTitles"
+
+class About extends Component {
+  render() {
+    return (
+      <SectionTitles title="About" />
+    )
+  }
+}
+
+module.exports = About;
