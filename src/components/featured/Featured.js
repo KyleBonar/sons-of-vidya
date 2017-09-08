2bcea3168a2ff56bc9757a3fa16fc83c4d484beb
Kyle Bonar <kylewb2011@gmail.com>
2017-09-08 12:35:01 -0500

    Abstacted video parts into separate component.

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import PropTypes from "prop-types"
 
 import SectionTitles from "../sectionTitles/SectionTitles";
+
+class GetVideo extends Component {
+  render() {
+    return (
+      <div className="latest-holder">
+        <span>{this.props.title}</span>
+        <div className="video-holder">
+          <iframe
+            id="ytplayer"
+            type="text/html"
+            src={`https://www.youtube.com/embed/${this.props
+              .videoID}?autoplay=0`}
+            frameBorder="0"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+GetVideo.propTypes = {
+  title: PropTypes.string.isRequired,
+  videoID: PropTypes.string.isRequired
+}
 
 class Featured extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      key: "AIzaSyALj1_1OHf-JhgtvZhl4xbKaY0A7GcjNd4"
+      key: "AIzaSyALj1_1OHf-JhgtvZhl4xbKaY0A7GcjNd4",
+      channelID: "UC7Lef3r2J8uA5PidtcOC3fQ",
+      latestVideoID: "",
+      sonsPickVideoID: "",
+      defaultVideoID: "u2lMI37CIk0"
     };
 
     this.getLatestYoutube = this.getLatestYoutube.bind(this);
@@ -22,34 +51,33 @@ class Featured extends Component {
       <section id="featured">
         <SectionTitles title="Featured Videos" />
         <div className="content container">
-          <div className="latest-holder">
-            <span>Sons' Pick</span>
-            <div className="video-holder">
-              <iframe
-                id="ytplayer"
-                type="text/html"
-                src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0&origin=http://example.com"
-                frameBorder="0"
-              />
-            </div>
-          </div>
-          <div className="latest-holder">
-            <span>Latest</span>
-            <div className="video-holder">
-              <iframe
-                id="ytplayer"
-                type="text/html"
-                src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=0&origin=http://example.com"
-                frameBorder="0"
-              />
-            </div>
-          </div>
+          <GetVideo
+            title="Sons Pick"
+            videoID={this.state.sonsPickVideoID || this.state.defaultVideoID}
+          />
+          <GetVideo
+            title="Latest Video"
+            videoID={this.state.latestVideoID || this.state.defaultVideoID}
+          />
         </div>
       </section>
     );
   }
 
-  getLatestYoutube() {}
+  getLatestYoutube() {
+    axios({
+      method: "get",
+      url: `https://www.googleapis.com/youtube/v3/search?key=${this.state
+        .key}&part=snippet&channelId=${this.state
+        .channelID}&order=date&maxResults=5`
+    })
+      .then(response => {
+        this.setState({
+          latestVideoID: response.data.items[0].id.videoId
+        });
+      })
+      .catch(err => console.log(err));
+  }
 }
 
 module.exports = Featured;
