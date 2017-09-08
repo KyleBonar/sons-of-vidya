2928d52b386443fd7eacb9f5dd199eb83a88e78b
Kyle Bonar <kylewb2011@gmail.com>
2017-09-08 12:35:31 -0500

    Included prop type checking.

--- src/components/sectionTitles/SectionTitles.js
+++ src/components/sectionTitles/SectionTitles.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class SectionTitles extends Component {
   render() {
@@ -16,5 +17,8 @@ class SectionTitles extends Component {
     );
   }
 }
+SectionTitles.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 module.exports = SectionTitles;
