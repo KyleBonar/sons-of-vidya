8f043ad0243a34d733ffb7694b9f9fbfea438106
Kyle Bonar <kylewb2011@gmail.com>
2017-09-11 16:05:03 -0500

    Component pulls in items from separate files and plugs each one into Items component.

--- src/components/playlist/Playlist.js
+++ src/components/playlist/Playlist.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 
 import SectionTitles from "../sectionTitles/SectionTitles";
+import PlaylistItems from "./PlaylistItems";
+
+class Item extends Component {
+  render() {
+    return (
+      <div>
+        <a href={this.props.link} target="_blank">
+          <img src={this.props.img} />
+          <span className="font-primary video-title">{this.props.title}</span>
+        </a>
+      </div>
+    );
+  }
+}
 
 class Playlist extends Component {
   render() {
     return (
       <section id="playlists">
         <SectionTitles title="Playlists" />
+        <div className="container">
+          {PlaylistItems.map((item, index) => (
+            <Item
+              title={item.title}
+              img={item.img}
+              link={item.link}
+              key={index}
+            />
+          ))}
+        </div>
       </section>
     );
   }
