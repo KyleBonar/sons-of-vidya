8f77ab323e3439242de8642bf3dc3757f244641d
Kyle Bonar <kylewb2011@gmail.com>
2017-09-11 16:05:19 -0500

    Init commit. This will be for playlist items only.

---
+++ src/components/playlist/PlaylistItems.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    title: "DeadlyDice! Week 1 Episode 1",
+    img: require("../../images/playlist_deadlyDiceS1E1_600x300.jpg"),
+    link:
+      "https://www.youtube.com/watch?v=niUBrphAG7E&list=PLBE7jgE02o_xAPpJ88IckKZfIxr6fMo4m"
+  },
+  {
+    title: "The Sons Play L.A. Noire AGAIN!",
+    img: require("../../images/playlist_LANOIRE_600x300.jpg"),
+    link:
+      "https://www.youtube.com/watch?v=pvY0ObyGUfQ&list=PLBE7jgE02o_zSO2FRBCX0la5HLJ5aXAl6"
+  },
+  {
+    title: "Golden Gloves Boxing!",
+    img: require("../../images/playlist_goldenGlovesBoxing_600x300.jpg"),
+    link:
+      "https://www.youtube.com/watch?v=eAD_ntyorA4&list=PL575FF5508EC239FD&index=8"
+  }
+];
