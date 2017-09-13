ce61951befcae8b662b0d4edfa478f2f20beae57
Kyle Bonar <kylewb2011@gmail.com>
2017-09-13 15:15:24 -0500

    Class for "rights" and pulling in the social buttons.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,25 @@
 import React, {Component} from "react";
 
+import {SocialButtons} from '../socialButtons/SocialButtons'
+
+class Rights extends Component {
+  render() {
+    return(
+      <div className="rights">
+        2017, All Rights Reserved
+      </div>
+    )
+  }
+}
+
 class Footer extends Component {
   render() {
-    return( <div>hi</div>)
+    return( <section id="footer">
+      <div className="container">
+        <Rights />
+        <SocialButtons id="footer-social"/>
+      </div>
+    </section>)
   }
 }
 
