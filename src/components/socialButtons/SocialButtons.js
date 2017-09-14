b2f6362cd90c4e13c1f94e4eebda7fee55638192
Kyle Bonar <kylewb2011@gmail.com>
2017-09-14 16:15:19 -0500

    Gave class name to each anchor tag and a more appropriate name for each image.

--- src/components/socialButtons/SocialButtons.js
+++ src/components/socialButtons/SocialButtons.js
@@ -9,23 +9,36 @@ export class SocialButtons extends Component {
     const Facebook = require("../../images/social_facebook_90x90.png");
     return (
       <div id={this.props.id}>
-        <a href="https://www.youtube.com/user/SonsOfVidya" target="_blank">
-          <img src={Youtube} className="social-link" />
+        <a
+          href="https://www.youtube.com/user/SonsOfVidya"
+          target="_blank"
+          className="social-item"
+        >
+          <img src={Youtube} className="social-img" />
         </a>
 
-        <a href="https://www.patreon.com/sonsofvidya" target="_blank">
-          <img src={Patreon} className="social-link" />
+        <a
+          href="https://www.patreon.com/sonsofvidya"
+          target="_blank"
+          className="social-item"
+        >
+          <img src={Patreon} className="social-img" />
         </a>
 
-        <a href="https://twitter.com/sonsofvidya" target="_blank">
-          <img src={Twitter} className="social-link" />
+        <a
+          href="https://twitter.com/sonsofvidya"
+          target="_blank"
+          className="social-item"
+        >
+          <img src={Twitter} className="social-img" />
         </a>
 
         <a
           href="https://www.facebook.com/Sons-Of-Vidya-256576031127242/"
           target="_blank"
+          className="social-item"
         >
-          <img src={Facebook} className="social-link" />
+          <img src={Facebook} className="social-img" />
         </a>
       </div>
     );
